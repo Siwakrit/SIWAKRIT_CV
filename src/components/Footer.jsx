1fcb1b0ea22d8f8f30e35e9f3aa67a7147b6937e
SIWAKRIT PATTAVEESIN <mr.siwakrit@gmail.com>
2024-11-16 02:31:29 +0700

    margin

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ import { WiAlien } from "react-icons/wi";
 const Footer = () => {
   return (
     <div className='flex justify-between items-center p-2 w-[100%]
-    border-t-[#7f8188]  border-t md:mt-5'>
+    border-t-[#7f8188]  border-t md:mt-[2rem]'>
         <ul>
             <li>
             <WiAlien className='hover:scale-110 hover:text-orange-500' size={60} />
