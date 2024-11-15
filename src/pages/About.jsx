32d8b9298ae249c57f8be06a03e303d729d3bccc
SIWAKRIT PATTAVEESIN <mr.siwakrit@gmail.com>
2024-11-16 02:29:14 +0700

    test

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -51,7 +51,7 @@ const About = () => {
   };
 
   return (
-    <section className="text-black w-[100%] mb-[2rem]" id="about">
+    <section className="text-black w-[100%] mb-[5rem] mt-[2rem]" id="about">
       <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
         <div className="flex justify-center items-center">
         <img src="/Img/Profile0.jpg" 
